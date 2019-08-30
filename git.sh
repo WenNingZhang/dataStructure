a82d5101457cbb2e23594816eda6ae5cf02fa606
wenning <zhangwenning@videopls.com>
2019-08-30 16:48:18 +0800

    clean

--- git.sh
+++ git.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 commit_message="$1"
 git add . 
-git commit -m "$commit_message" --amend --author="zhangwinning <1527841714@qq.com>"
-git pull
+git commit -m "$commit_message" 
 git push
